bad080033daca6ab0c06f8e87de4ac98585bce00
LFIT16 <ibarratucano@gmail.com>
2025-06-10 02:56:41 -0500

    mejoras

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -14,7 +14,6 @@ export const ROUTES: RouteInfo[] = [
     { path: '/passwords/list', title: 'Contraseñas',  icon:'unarchive', class: 'active-pro' },
     { path: '/addresses/list', title: 'Dirección',  icon:'location_on', class: '' },
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
     {
       path: '/devices/list',
       title: 'Dispositivos',
@@ -34,13 +33,9 @@ export const ROUTES: RouteInfo[] = [
       class: ''
     },
     { path: '/digital-signatures/list', title: 'Firmas Digitales',  icon: 'draw', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    // Nuevos accesos directos:
     { path: '/profiles/list', title: 'Perfiles', icon: 'account_circle', class: '' },
     { path: '/permissions/list', title: 'Permisos', icon: 'vpn_key', class: '' },
     { path: '/role-permissions/list', title: 'Rol-Permiso', icon: 'supervisor_account', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
 ];
 
 @Component({
