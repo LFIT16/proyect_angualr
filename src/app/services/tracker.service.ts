d91ac9d625372eb410c7304c853c4de2d182a402
Solorza <david.josue.solorza@gmail.com>
2025-06-10 13:00:07 -0500

    alcenamiento en el local

--- src/app/services/tracker.service.ts
+++ src/app/services/tracker.service.ts
@@ -5,29 +5,45 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TrackerService {
-  private counters: { [key: string]: number } = {};
 
   constructor() {}
 
   track(route: string): void {
-    if (!this.counters[route]) {
-      this.counters[route] = 1;
+    // Leer lo que ya hay en el localStorage
+    const data = localStorage.getItem('routeCounts');
+    const routeCounts = data ? JSON.parse(data) : {};
+
+    // Aumentar el contador de la ruta
+    if (routeCounts[route]) {
+      routeCounts[route]++;
     } else {
-      this.counters[route]++;
+      routeCounts[route] = 1;
     }
+
+    // Guardar de nuevo en el localStorage
+    localStorage.setItem('routeCounts', JSON.stringify(routeCounts));
   }
 
   getMostVisited(): string {
+    const data = localStorage.getItem('routeCounts');
+    const routeCounts = data ? JSON.parse(data) : {};
+
     let max = 0;
     let mostVisited = '';
 
-    for (const key in this.counters) {
-      if (this.counters[key] > max) {
-        max = this.counters[key];
+    for (const key in routeCounts) {
+      if (routeCounts[key] > max) {
+        max = routeCounts[key];
         mostVisited = key;
       }
     }
 
     return mostVisited;
+  }
+
+  // (Opcional) Si quieres ver todas las rutas con sus visitas:
+  getAllCounts(): { [key: string]: number } {
+    const data = localStorage.getItem('routeCounts');
+    return data ? JSON.parse(data) : {};
   }
 }
